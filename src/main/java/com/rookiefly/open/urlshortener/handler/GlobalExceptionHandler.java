33abcc278486b4b3809da051f04302e7c35b40be
王高翔 <frankwanggaoxiang@didiglobal.com>
2020-11-10 19:37:00 +0800

    add prometheus

--- src/main/java/com/rookiefly/open/urlshortener/handler/GlobalExceptionHandler.java
+++ src/main/java/com/rookiefly/open/urlshortener/handler/GlobalExceptionHandler.java
@@ -1,9 +1,11 @@
 package com.rookiefly.open.urlshortener.handler;
 
 import com.rookiefly.open.urlshortener.monitor.PrometheusCustomMonitor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import javax.annotation.Resource;
 
@@ -14,6 +16,7 @@ public class GlobalExceptionHandler {
     private PrometheusCustomMonitor monitor;
 
     @ResponseBody
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(value = Exception.class)
     public String handle(Exception e) {
         monitor.getRequestErrorCount().increment();
