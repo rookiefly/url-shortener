6e41c7407cab05d592acf6dd2600c010e6e9e380
rookiefly <datouxiangzi@163.com>
2021-07-14 15:50:57 +0800

    重构代码

--- src/main/java/com/rookiefly/open/urlshortener/UrlShortenerApplication.java
+++ src/main/java/com/rookiefly/open/urlshortener/UrlShortenerApplication.java
@@ -1,21 +1,36 @@
 package com.rookiefly.open.urlshortener;
 
-import org.springframework.boot.CommandLineRunner;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Optional;
 
 @SpringBootApplication
 @EnableCaching
-public class UrlShortenerApplication implements CommandLineRunner {
+@Slf4j
+public class UrlShortenerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(UrlShortenerApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) {
-
+    public static void main(String[] args) throws UnknownHostException {
+        ConfigurableApplicationContext application = SpringApplication.run(UrlShortenerApplication.class, args);
+        Environment env = application.getEnvironment();
+        String contextPath = env.getProperty("server.servlet.context-path");
+        contextPath = Optional.ofNullable(contextPath).orElse("").replaceFirst("/", "");
+        contextPath = (contextPath.length() <= 0 || contextPath.endsWith("/")) ? contextPath : contextPath + "/";
+        String hostAddress = InetAddress.getLocalHost().getHostAddress();
+        String serverPort = env.getProperty("server.port");
+        String urlCtx = hostAddress + ":" + serverPort + "/" + contextPath;
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "\t\t地址列表\n\t" +
+                        "管理地址：http://{}\n" +
+                        "----------------------------------------------------------",
+                urlCtx
+        );
     }
 
 /*    @Bean
